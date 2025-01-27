399b79a425f35a562ef6e914027bf23f10654d2e
MLC Bloeiman <mar@strawmelonjuice.com>
2025-01-27 12:34:36 +0100

    Remove echo

--- cynthia_websites_mini_server/src/cynthia_websites_mini_server/utils/prompts_ffi.ts
+++ cynthia_websites_mini_server/src/cynthia_websites_mini_server/utils/prompts_ffi.ts
@@ -5,7 +5,6 @@ export function promptForInput(
   msg: string,
 ): GleamOk<string, unknown> | GleamError<unknown, null> {
   const answer = prompt(msg);
-  console.log(answer);
   if (answer === null) {
     return new GleamError(null);
   } else {
