e9ff8713b205f0030dc2b94e99d0e822455c7ab5
MLC Bloeiman <mar@strawmelonjuice.com>
2025-03-07 22:44:39 +0100

    Move everything out of the class, making it an interface, for direct ffi call access.

--- cynthia_websites_mini_client/src/cynthia_websites_mini_client/datamanagement_ffi.ts
+++ cynthia_websites_mini_client/src/cynthia_websites_mini_client/datamanagement_ffi.ts
@@ -16,12 +16,13 @@ interface flatGlobalConfig {
   global_site_description: string;
 }
 
-class ClientStore {
-  private db: Database;
-  constructor(global_config: flatGlobalConfig) {
-    // Creates database, this step might take some time.
-    this.db = new SQL.Database();
-    let sqlstr = `
+interface ClientStore {
+  db: Database;
+}
+
+export function initialise(config: flatGlobalConfig): ClientStore {
+  let obj: ClientStore = { db: new SQL.Database() };
+  let sqlstr = `
       CREATE TABLE IF NOT EXISTS lasthash (
         hash TEXT NOT NULL PRIMARY KEY
       );
@@ -52,209 +53,29 @@ class ClientStore {
         last_inserted_at TEXT NOT NULL,
         original_filename TEXT NOT NULL PRIMARY KEY
       );
-      INSERT INTO globalconfig(theme, theme_dark, colour, site_name, site_description) VALUES('${global_config.global_theme}', '${global_config.global_theme_dark}', '${global_config.global_colour}', '${global_config.global_site_name}', '${global_config.global_site_description}');
+      INSERT INTO globalconfig(theme, theme_dark, colour, site_name, site_description) VALUES('${config.global_theme}', '${config.global_theme_dark}', '${config.global_colour}', '${config.global_site_name}', '${config.global_site_description}');
       `;
-    this.db.run(sqlstr);
-  }
-  update_lasthash(hash: string) {
-    this.db.run("DELETE FROM lasthash;");
-    this.db.run(`INSERT INTO lasthash(hash) VALUES('${hash}');`);
-  }
-  get_lasthash() {
-    let res = this.db.exec("SELECT * FROM lasthash;");
-    if (res.length == 0) return "/";
-    return res[0].values[0][0]?.toString();
-  }
-  fetch_content_from_clientstore_by_permalink(
-    store: ClientStore,
-    permalink: string,
-  ) {
-    let res = store.db.exec(
-      "SELECT * FROM content WHERE meta_permalink = '" + permalink + "';",
-    );
-    if (res.length == 0) return undefined;
-    let row = res[0].values[0];
-    if (row.includes(null)) return undefined;
-    let content = {
-      html: row[0] as string,
-      original_filename: row[1] as string,
-      meta_title: row[2] as string,
-      meta_description: row[3] as string,
-      meta_kind: (() => {
-        switch (row[4]!.toString()) {
-          case "0":
-            return "page";
-          case "1":
-            return "post";
-          default:
-            return "page";
-        }
-      })(),
-      meta_permalink: row[5] as string,
-      last_inserted_at: row[6] as string,
-      meta_in_menus: (row[7] as string).split(",").map((x) => parseInt(x)),
-    };
-    return content;
-  }
-  return_content_store_as_array() {
-    let res = this.db.exec("SELECT * FROM content;");
-    if (res.length == 0) return [];
-    let rows = res[0].values;
-    if (rows.length == 0) return [];
-    let content_store: {
-      html: string;
-      original_filename: string;
-      meta_title: string;
-      meta_description: string;
-      meta_kind: string;
-      meta_permalink: string;
-      last_inserted_at: string;
-      meta_in_menus: number[];
-    }[] = [];
-    for (let row of rows) {
-      // But what if there's only one menu item? or zero?
-      let meta_in_menus = (row[7] as string).split(",").map((x) => parseInt(x));
-      content_store.push({
-        html: row[0] as string,
-        original_filename: row[1] as string,
-        meta_title: row[2] as string,
-        meta_description: row[3] as string,
-        meta_kind: (() => {
-          switch (row[4]!.toString()) {
-            case "0":
-              return "page";
-            case "1":
-              return "post";
-            default:
-              return "page";
-          }
-        })(),
-        meta_permalink: row[5] as string,
-        last_inserted_at: row[6] as string,
-        meta_in_menus,
-      });
-    }
-    return content_store;
-  }
-  add_to_content_queue(content: {
-    meta_title: string;
-    meta_description: string;
-    meta_kind: number;
-    meta_permalink: string;
-    last_inserted_at: string;
-    original_filename: string;
-  }) {
-    let sql = `
-      INSERT INTO contentqueue(meta_title, meta_description, meta_kind, meta_permalink, last_inserted_at, original_filename) VALUES('${content.meta_title}', '${content.meta_description}', ${content.meta_kind}, '${content.meta_permalink}', '${content.last_inserted_at}', '${content.original_filename}');
-    `;
-    this.db.run(sql);
-  }
-  add_to_content_store(content: {
-    html: string;
-    original_filename: string;
-    meta_title: string;
-    meta_description: string;
-    meta_kind: number;
-    meta_permalink: string;
-    last_inserted_at: string;
-    meta_in_menus: number[];
-  }) {
-    let stmt = this.db.prepare(
-      "INSERT INTO content(html, original_filename, meta_title, meta_description, meta_kind, meta_permalink, last_inserted_at, meta_in_menus) VALUES(?, ?, ?, ?, ?, ?, ?, ?);",
-    );
-    stmt.run([
-      content.html,
-      content.original_filename,
-      content.meta_title,
-      content.meta_description,
-      content.meta_kind,
-      content.meta_permalink,
-      content.last_inserted_at,
-      content.meta_in_menus.join(","),
-    ]);
-  }
-  content_queue_next(
-    cb: (
-      arg0:
-        | {
-            meta_title: string;
-            meta_description: string;
-            meta_kind: number;
-            meta_permalink: string;
-            last_inserted_at: string;
-            original_filename: string;
-          }
-        | undefined,
-    ) => void,
-  ) {
-    let res = this.db.exec("SELECT * FROM contentqueue LIMIT 1;");
-    if (res.length == 0) return undefined;
-    let row = res[0].values[0];
-    // If any of the values are null, return undefined
-    if (row.includes(null)) {
-      console.log(
-        "One or more queued content items are not readable. Skipping.",
-      );
-      return undefined;
-    }
-    let content = {
-      meta_title: row[0] as string,
-      meta_description: row[1] as string,
-      meta_kind: row[2] as number,
-      meta_permalink: row[3] as string,
-      last_inserted_at: row[4] as string,
-      original_filename: row[5] as string,
-    };
-    // Check if the content is already in the store
-    if (
-      !(
-        this.db.exec(
-          "SELECT * FROM content WHERE original_filename = '" +
-            content.original_filename +
-            "';",
-        ).length > 0
-      )
-    ) {
-      cb(content);
-    }
-    // Delete from queue
-    this.db.run(
-      "DELETE FROM contentqueue WHERE original_filename = '" +
-        content.original_filename +
-        "';",
-    );
-  }
-  get_config_item(item: string): string[] {
-    let res = this.db.exec(`SELECT DISTINCT ${item}  FROM globalconfig`);
-    let c = res[0].values[0][0];
-    // console.log(item, ":", c);
-    if (c == undefined) return [];
-    return [c.toString()];
-  }
-  update(global_config: flatGlobalConfig) {
-    let sql = `
-      DELETE FROM globalconfig;
-      INSERT INTO globalconfig(theme, theme_dark, colour, site_name, site_description) VALUES('${global_config.global_theme}', '${global_config.global_theme_dark}', '${global_config.global_colour}', '${global_config.global_site_name}', '${global_config.global_site_description}');
-  `;
-    this.db.run(sql);
-  }
-}
-
-export function initialise(config: flatGlobalConfig) {
-  // console.log("Initialising store with config", config);
-  const store = new ClientStore(config);
-  return store;
+  obj.db.run(sqlstr);
+  return obj;
 }
 
 export function populate_global_config(
   store: ClientStore,
   config: flatGlobalConfig,
 ): void {
-  store.update(config);
+  let sql = `
+      DELETE FROM globalconfig;
+      INSERT INTO globalconfig(theme, theme_dark, colour, site_name, site_description) VALUES('${config.global_theme}', '${config.global_theme_dark}', '${config.global_colour}', '${config.global_site_name}', '${config.global_site_description}');
+  `;
+  store.db.run(sql);
 }
 
 export function get_config_item(store: ClientStore, item: string): string[] {
-  return store.get_config_item(item);
+  let res = store.db.exec(`SELECT DISTINCT ${item}  FROM globalconfig`);
+  let c = res[0].values[0][0];
+  // console.log(item, ":", c);
+  if (c == undefined) return [];
+  return [c.toString()];
 }
 
 export function add_to_content_queue(
@@ -268,7 +89,10 @@ export function add_to_content_queue(
     original_filename: string;
   },
 ): void {
-  store.add_to_content_queue(content);
+  let sql = `
+      INSERT INTO contentqueue(meta_title, meta_description, meta_kind, meta_permalink, last_inserted_at, original_filename) VALUES('${content.meta_title}', '${content.meta_description}', ${content.meta_kind}, '${content.meta_permalink}', '${content.last_inserted_at}', '${content.original_filename}');
+    `;
+  store.db.run(sql);
 }
 export function add_to_content_store(
   store: ClientStore,
@@ -283,7 +107,19 @@ export function add_to_content_store(
     meta_in_menus: number[];
   },
 ): void {
-  store.add_to_content_store(content);
+  let stmt = store.db.prepare(
+    "INSERT INTO content(html, original_filename, meta_title, meta_description, meta_kind, meta_permalink, last_inserted_at, meta_in_menus) VALUES(?, ?, ?, ?, ?, ?, ?, ?);",
+  );
+  stmt.run([
+    content.html,
+    content.original_filename,
+    content.meta_title,
+    content.meta_description,
+    content.meta_kind,
+    content.meta_permalink,
+    content.last_inserted_at,
+    content.meta_in_menus.join(","),
+  ]);
 }
 export function next_in_content_queue(
   store: ClientStore,
@@ -300,15 +136,93 @@ export function next_in_content_queue(
       | undefined,
   ) => void,
 ) {
-  store.content_queue_next(cb);
+  let res = store.db.exec("SELECT * FROM contentqueue LIMIT 1;");
+  if (res.length == 0) return undefined;
+  let row = res[0].values[0];
+  // If any of the values are null, return undefined
+  if (row.includes(null)) {
+    console.log("One or more queued content items are not readable. Skipping.");
+    return undefined;
+  }
+  let content = {
+    meta_title: row[0] as string,
+    meta_description: row[1] as string,
+    meta_kind: row[2] as number,
+    meta_permalink: row[3] as string,
+    last_inserted_at: row[4] as string,
+    original_filename: row[5] as string,
+  };
+  // Check if the content is already in the store
+  if (
+    !(
+      store.db.exec(
+        "SELECT * FROM content WHERE original_filename = '" +
+          content.original_filename +
+          "';",
+      ).length > 0
+    )
+  ) {
+    cb(content);
+  }
+  // Delete from queue
+  store.db.run(
+    "DELETE FROM contentqueue WHERE original_filename = '" +
+      content.original_filename +
+      "';",
+  );
 }
 // Dict(Int, List(#(String, String)))
 export function get_menu_items(
   store: ClientStore,
 ): Map<number, [string, string]> {
+  let content_store: {
+    html: string;
+    original_filename: string;
+    meta_title: string;
+    meta_description: string;
+    meta_kind: string;
+    meta_permalink: string;
+    last_inserted_at: string;
+    meta_in_menus: number[];
+  }[] = [];
+  {
+    let res = store.db.exec("SELECT * FROM content;");
+    if (res.length == 0) content_store = [];
+    else {
+      let rows = res[0].values;
+      if (rows.length == 0) content_store = [];
+      else {
+        for (let row of rows) {
+          // But what if there's only one menu item? or zero?
+          let meta_in_menus = (row[7] as string)
+            .split(",")
+            .map((x) => parseInt(x));
+          content_store.push({
+            html: row[0] as string,
+            original_filename: row[1] as string,
+            meta_title: row[2] as string,
+            meta_description: row[3] as string,
+            meta_kind: (() => {
+              switch (row[4]!.toString()) {
+                case "0":
+                  return "page";
+                case "1":
+                  return "post";
+                default:
+                  return "page";
+              }
+            })(),
+            meta_permalink: row[5] as string,
+            last_inserted_at: row[6] as string,
+            meta_in_menus,
+          });
+        }
+      }
+    }
+  }
   let v: string[][][] = [[], [], [], [], []];
-  store.return_content_store_as_array().forEach((item, _) => {
-    if (item.meta_kind == 1) return;
+  content_store.forEach((item, _) => {
+    if (item.meta_kind == "post") return;
     if (item.meta_in_menus.includes(1))
       v[0].push([item.meta_title, item.meta_permalink]);
     if (item.meta_in_menus.includes(2))
@@ -336,20 +250,49 @@ export function fetch_content_from_clientstore_by_permalink(
   // Remove trailing slash from permalink if it exists
   permalink = permalink.replace(/\/$/, "");
   // console.log(list_all_permalinks_in_contentstore(store));
-  const po = store.fetch_content_from_clientstore_by_permalink(
-    store,
-    permalink,
-  );
-  if (po == undefined) return new Gleam.Error("");
-  return new Gleam.Ok(po);
+  const output = (() => {
+    let res = store.db.exec(
+      "SELECT * FROM content WHERE meta_permalink = '" + permalink + "';",
+    );
+    if (res.length == 0) return undefined;
+    let row = res[0].values[0];
+    if (row.includes(null)) return undefined;
+    let content = {
+      html: row[0] as string,
+      original_filename: row[1] as string,
+      meta_title: row[2] as string,
+      meta_description: row[3] as string,
+      meta_kind: (() => {
+        switch (row[4]!.toString()) {
+          case "0":
+            return "page";
+          case "1":
+            return "post";
+          default:
+            return "page";
+        }
+      })(),
+      meta_permalink: row[5] as string,
+      last_inserted_at: row[6] as string,
+      meta_in_menus: (row[7] as string).split(",").map((x) => parseInt(x)),
+    };
+    return content;
+  })();
+  if (output == undefined) return new Gleam.Error("");
+  return new Gleam.Ok(output);
 }
 
 export function update_lasthash(store: ClientStore, hash: string) {
-  store.update_lasthash(hash);
+  store.db.run("DELETE FROM lasthash;");
+  store.db.run(`INSERT INTO lasthash(hash) VALUES('${hash}');`);
 }
 
 export function get_lasthash(store: ClientStore) {
-  let lel = store.get_lasthash();
+  let lel = (() => {
+    let res = store.db.exec("SELECT * FROM lasthash;");
+    if (res.length == 0) return "/";
+    return res[0].values[0][0]?.toString();
+  })();
   if (lel == undefined) return new Gleam.Error("");
   return new Gleam.Ok(lel);
 }
@@ -357,7 +300,51 @@ export function get_lasthash(store: ClientStore) {
 export function list_all_permalinks_in_contentstore(
   store: ClientStore,
 ): string[] {
-  let content_store = store.return_content_store_as_array();
+  let content_store: {
+    html: string;
+    original_filename: string;
+    meta_title: string;
+    meta_description: string;
+    meta_kind: string;
+    meta_permalink: string;
+    last_inserted_at: string;
+    meta_in_menus: number[];
+  }[] = [];
+  {
+    let res = store.db.exec("SELECT * FROM content;");
+    if (res.length == 0) content_store = [];
+    else {
+      let rows = res[0].values;
+      if (rows.length == 0) content_store = [];
+      else {
+        for (let row of rows) {
+          // But what if there's only one menu item? or zero?
+          let meta_in_menus = (row[7] as string)
+            .split(",")
+            .map((x) => parseInt(x));
+          content_store.push({
+            html: row[0] as string,
+            original_filename: row[1] as string,
+            meta_title: row[2] as string,
+            meta_description: row[3] as string,
+            meta_kind: (() => {
+              switch (row[4]!.toString()) {
+                case "0":
+                  return "page";
+                case "1":
+                  return "post";
+                default:
+                  return "page";
+              }
+            })(),
+            meta_permalink: row[5] as string,
+            last_inserted_at: row[6] as string,
+            meta_in_menus,
+          });
+        }
+      }
+    }
+  }
   let permalinks: string[] = [];
   content_store.forEach((item, _) => {
     permalinks.push(item.meta_permalink);
